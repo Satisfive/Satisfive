2f57ee329e3524fad1530fe4324b2718755e5c90
Dorian <dorian@doma.io>
2016-02-13 12:25:17 -0800

    try to fix it

--- app.js
+++ app.js
@@ -10,22 +10,20 @@ var express = require('express')
   , path = require('path');
 
 var app = express();
+var env = process.env.NODE_ENV || 'development';
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
-});
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.configure('development', function(){
+if ('development' == env) {
   app.use(express.errorHandler());
-});
+}
 
 app.get('/', routes.index);
 app.get('/users', user.list);
