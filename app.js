21ac97572b21ccf1fe2385c5caf689968e3620e8
eli <eli.sakov@hotmail.com>
2016-02-13 14:34:00 -0800

    app

---
+++ app.js
@@ -0,0 +1,54 @@
+var express = require('express')
+  , http = require('http')
+  , sparkpost = require('./sparkpost')
+  , db = require('./db')
+  , path = require('path');
+
+var app = express();
+
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/api/emails/:uuid', function(req, res){
+  var email = db.Email.find(req.params.uuid);
+  if (email) {
+    res.json(email);
+  } else {
+    res.send(404);
+  }
+});
+
+app.post('/users', function(req, res){
+  var user;
+  if (user = User.find(req.params.email)) {
+    res.send(200);
+  } else {
+    res.send(400);
+  }
+
+});
+
+app.get('/users/:email', function(req, res){
+  res.send('s');
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
