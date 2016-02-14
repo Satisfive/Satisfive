138a53a88a7d1831934b3dda1cc7e3a63f912ad7
eli <eli.sakov@hotmail.com>
2016-02-13 20:50:53 -0800

    iterated on webhook

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.post('/webhook', function(req, res){
   }).forEach(function(msg){
     var  msg, thisEmail = "eli.sakov@hotmail.com"; // msg.rcpt_to ,
     msg.content.from = msg.msg_from;
-    delete(msg.content.headers);
+    msg.content.headers = undefined;
     msg = new sparkpost.Message({
         content: msg.content
     });
